d54491d045ef1118640c6baf0bc51d839bd678ce
Matthias Gerwin <matthiasjger@gmail.com>
2019-11-15 19:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-function shout(string) {
-  return string.toUpperCase("hello!")
-}
 
-function whisper(string) {
-  return string.toLowerCase("HELLO")
-}
 
-function logShout(string) {
-  console.log(string.toUpperCase("hello!"))
-}
